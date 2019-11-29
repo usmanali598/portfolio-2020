7771454cd0ef1fc2454ccd2a3f96446b8dc0a6e0
Usman Ali <usman_pervaiz@live.com>
2019-11-29 22:08:13 +0200

    Updates

---
+++ precache-manifest.8a241c7c86e44d91a9b9030a40115ba5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16751f5abe7b4dad6d9f53256487c54",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "980dc3cf4d72b0e27706",
+    "url": "/portfolio-2020/static/css/2.20225cc9.chunk.css"
+  },
+  {
+    "revision": "98f00ea74c50b8736ef0",
+    "url": "/portfolio-2020/static/css/main.44e5906c.chunk.css"
+  },
+  {
+    "revision": "980dc3cf4d72b0e27706",
+    "url": "/portfolio-2020/static/js/2.55e50fd9.chunk.js"
+  },
+  {
+    "revision": "98f00ea74c50b8736ef0",
+    "url": "/portfolio-2020/static/js/main.68994fa0.chunk.js"
+  },
+  {
+    "revision": "44495422171dc8e2674f",
+    "url": "/portfolio-2020/static/js/runtime-main.3d978818.js"
+  },
+  {
+    "revision": "1a4d60ab9452fb666f1c11f19529743e",
+    "url": "/portfolio-2020/static/media/akt.1a4d60ab.jpeg"
+  },
+  {
+    "revision": "4c18ce0df0a17cc3ebf4e5a0eb1e99a3",
+    "url": "/portfolio-2020/static/media/anr.4c18ce0d.png"
+  },
+  {
+    "revision": "087cd08e3a2c7ba967dfe883eefc4fb9",
+    "url": "/portfolio-2020/static/media/croisson.087cd08e.png"
+  },
+  {
+    "revision": "11b8b6441c546d88024653a79cc877c8",
+    "url": "/portfolio-2020/static/media/cup.11b8b644.png"
+  },
+  {
+    "revision": "acbff11657fbbdd9b9aeb8ceaf396344",
+    "url": "/portfolio-2020/static/media/finkin.acbff116.png"
+  },
+  {
+    "revision": "e7a09b9c69300f8caf92da3333ffd3bb",
+    "url": "/portfolio-2020/static/media/laptop.e7a09b9c.png"
+  },
+  {
+    "revision": "97cbdc6f5dd13937efeb1029f2e83004",
+    "url": "/portfolio-2020/static/media/neyrk.97cbdc6f.png"
+  },
+  {
+    "revision": "d29a6b4dcbceb36b02107b53b4fcb987",
+    "url": "/portfolio-2020/static/media/reit.d29a6b4d.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio-2020/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio-2020/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio-2020/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio-2020/static/media/slick.f97e3bbf.svg"
+  }
+]);
